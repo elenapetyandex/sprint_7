c2f854c32110fa63788a7fa0bbeb2971807ad55d
Elena Pet <dragonqueen3@yandex.ru>
2025-03-28 14:56:45 +0700

    replace generated data to helpers.py

--- data.py
+++ data.py
@@ -1,7 +1,3 @@
-import requests
-import random
-import string
-
 
 class Data:
 
@@ -14,43 +10,5 @@ class Data:
     data_for_create_order_id_courier_known = [["11", 1, 0], [None, 2, 0], ["6", 29, 0], ["1", 30, 0]]
     data_for_create_order_id_courier_unknown = [["112", 10, 0]]
 
-    @staticmethod
-    def generate_string():
-        letters = string.ascii_lowercase
-        random_string = ''.join(random.choice(letters) for i in range(10))
-        return random_string
 
-    @staticmethod
-    def register_new_courier_and_return_login_password():
-        # метод генерирует строку, состоящую только из букв нижнего регистра, в качестве параметра передаём длину строки
-        def generate_random_string(length):
-            letters = string.ascii_lowercase
-            random_string = ''.join(random.choice(letters) for i in range(length))
-            return random_string
 
-        # создаём список, чтобы метод мог его вернуть
-        login_pass = []
-
-        # генерируем логин, пароль и имя курьера
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
-
-        # собираем тело запроса
-        payload = {
-            "login": login,
-            "password": password,
-            "firstName": first_name
-        }
-
-        # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
-        response = requests.post('https://qa-scooter.praktikum-services.ru/api/v1/courier', data=payload)
-
-        # если регистрация прошла успешно (код ответа 201), добавляем в список логин и пароль курьера
-        if response.status_code == 201:
-            login_pass.append(login)
-            login_pass.append(password)
-            login_pass.append(first_name)
-
-        # возвращаем список
-        return login_pass
